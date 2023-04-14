e69b1420cf9d2028de9b5423727043ebf1eed179
Jean Hamm <jeanmichehamm@gmail.com>
2023-04-14 17:59:17 -0300

    PC da empresa

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-function tocaSomPom(){
-    const listaDeSom = document.querySelectorAll('audio')
-    listaDeSom[0].play();
+function tocaSom(idElementoAudio){
+    document.querySelectorAll(idElementoAudio).play();
 }
-
+//o querySelector quando está no all, e procura algo amplo ele vai ser identificado depois com o [0, 1, 2...], recomendo passar para uma variavel ou constante esse atributo para manipular melhor
 const listaDeTecla = document.querySelectorAll('.tecla');
 
 let contador = 0;
+// class list é usado para acessar qual classe vai puxar
+listaDeTecla[0].classList[1]
 
 while(contador < listaDeTecla.length){
-    
-    listaDeTecla[contador].onclick = tocaSomPom;
+    const tecla = listaDeTecla[contador];
+    const instrumento = tecla.classList[1];
+
+    // Como dentro da função tocaSom eu preciso usar o '' dentro dos parenteses, usando os `` $ {} conseguimos modificar a string para usar na função tocaSom (Chamado de template String)
+    const idAudio = `#som_${instrumento}`;
+
+    //Foi colocada a função anonima para não executar a tocaSom antes do clique no botão
+    tecla.onclick = function() {
+         tocaSom(idAudio);
+    };
 
     contador++;
 
---
+++ main2.js
@@ -0,0 +1,9 @@
+
+
+
+function tocaSom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+document.querySelector('.tecla_pom').onclick = tocaSom;
